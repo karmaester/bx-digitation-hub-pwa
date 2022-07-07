2dbc40798a2e3fe59748d29e2fbc0dbf56e25804
Ariel Armijo <arielarmijo@yahoo.es>
2022-07-07 11:13:35 -0400

    Link store with app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
-import '../styles/global.scss'
-import '../styles/mixins.scss'
+import { Provider } from 'react-redux';
+import store from 'redux/store';
+import 'styles/global.scss'
+import 'styles/mixins.scss'
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
