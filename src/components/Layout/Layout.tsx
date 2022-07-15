a6d3398d8d46802a214a23c8dfb0bdcf2c012a31
Ariel Armijo <ariel@kranio.io>
2022-07-15 11:49:23 -0400

    Add partial blue colors pallete

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,22 +2,14 @@ import Head from 'next/head';
 import Header from '@components/molecules/Header';
 
 export default function Layout({ children }: { children: JSX.Element }) {
-
-    return (
-        <>
-            <Head>
-                <title>Digitación Hub</title>
-                <link rel="icon" href="/LOGO-solo-icono.svg" />
-            </Head>
-            <Header />
-            <main>{children}</main>
-            <style global jsx>{`
-        body {
-          margin: 0;
-          font-family: 'SF Pro Text', 'SF Pro Icons', 'Helvetica Neue', 'Helvetica',
-      'Arial', sans-serif;
-        }
-      `}</style>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Digitación Hub</title>
+        <link rel='icon' href='/LOGO-solo-icono.svg' />
+      </Head>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { Provider } from 'react-redux';
 import store from 'redux/store';
 import 'styles/global.scss'
-import 'styles/mixins.scss'
 
 export default function MyApp({ Component, pageProps }: any) {
   // Use the layout defined at the page level, if available
