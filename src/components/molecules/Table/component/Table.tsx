b6bc5fc3387c92faa13508ed02ba1c05a5048552
Ariel Armijo <ariel@kranio.io>
2022-07-18 16:12:09 -0400

    Fix react hydration error

--- src/components/molecules/Table/component/Table.tsx
+++ src/components/molecules/Table/component/Table.tsx
@@ -1,35 +1,40 @@
-import React from 'react'
-import styles from '../styles/Table.module.scss'
+import React from 'react';
+import styles from '../styles/Table.module.scss';
 
-const columns = ["Item", "OS", "EV-EX"]
+const columns = ['Item', 'OS', 'EV-EX'];
 
 type oSItemT = {
-    os: string,
-    "ev-ex": string
-}
+  os: string;
+  'ev-ex': string;
+};
 
 type TableT = {
-    data?: oSItemT[] | [],
-}
+  data?: oSItemT[] | [];
+};
 
 const Table = ({ data }: TableT) => {
+  return (
+    <div className={styles.tableContainer}>
+      <table>
+        <thead>
+          <tr>
+            {columns.map((item) => (
+              <th key={item}>{item}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((item, idx) => (
+            <tr key={idx}>
+              <td>{idx + 1}</td>
+              <td>{item.os}</td>
+              <td>{item['ev-ex']}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-    return (
-        <div className={styles.tableContainer}>
-            <table>
-                <tr>
-                    {columns.map(item => <th key={item}>{item}</th>)}
-                </tr>
-                {data?.map((item, idx) => (
-                    <tr key={idx}>
-                        <td>{idx + 1}</td>
-                        <td>{item.os}</td>
-                        <td>{item['ev-ex']}</td>
-                    </tr>
-                ))}
-            </table>
-        </div>
-    )
-}
-
-export default Table
+export default Table;
